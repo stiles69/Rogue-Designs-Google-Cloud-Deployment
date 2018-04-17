96852c23c995153ff1fe17c5d2733ab5aa86cf64
stiles69 <brett.salemink@gmail.com>
2018-04-17 09:00:12 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![Image of Stiles](https://storage.googleapis.com/stiles-images/StilesLogo.png)
+# roguedesigns-190602-google-cloud
+Website upload to Google Cloud with GoLang.
---
+++ Webserver1/files/precache-manifest.bea2d91e8eefa0c2ee44493d3c26b331.js
@@ -0,0 +1,103 @@
+self.__precacheManifest = [
+  {
+    "revision": "28a7574913b746f3d72ed35a376c4d4e",
+    "url": "/static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "url": "/static/js/app.9a7ee0ef52bde0703f90.js"
+  },
+  {
+    "url": "/static/js/vendor.3c8e587d7ba8acd7b6a2.js"
+  },
+  {
+    "url": "/static/js/manifest.2ae2e69a05c33dfc65f8.js"
+  },
+  {
+    "revision": "d3eee718cd8f70c508b8b9ff008d2092",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "b562bbf0b75458b5ad131ac8d446acbd",
+    "url": "/static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "396936f86f79f2b1546c3677ed989ca2",
+    "url": "/static/img/Logo-192x192.svg"
+  },
+  {
+    "revision": "ec6d3ec4c4acab500e1ed36ded7e5dfc",
+    "url": "/static/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1ab1a1fcab0ac52b1dcf5404d041629b",
+    "url": "/static/img/favicon.ico"
+  },
+  {
+    "revision": "882879defbb6c480eaa4c966e6f0d19e",
+    "url": "/static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "fb5dd3084aaaae63ba1875d1804b8b44",
+    "url": "/static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "801f659da85e7ea62b58dcde310182ad",
+    "url": "/static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "cdb326107f0c497a9942c6405a390b31",
+    "url": "/static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "ca1c7468738765d72d41dce0f64e7638",
+    "url": "/static/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "6e78ab3146b43b2e314ef277a914671d",
+    "url": "/static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "28451742d48b5867f55ff4fd981e3389",
+    "url": "/static/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "882879defbb6c480eaa4c966e6f0d19e",
+    "url": "/static/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "afd33109e7c622e4440b043255dca19e",
+    "url": "/static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "07ee6703826cdc035dc3adf1cd21eaa7",
+    "url": "/static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "1ab1a1fcab0ac52b1dcf5404d041629b",
+    "url": "/static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "882879defbb6c480eaa4c966e6f0d19e",
+    "url": "/static/img/Logo-192x192.png"
+  },
+  {
+    "revision": "882879defbb6c480eaa4c966e6f0d19e",
+    "url": "/static/img/Logo-192x192.882879d.png"
+  },
+  {
+    "revision": "396936f86f79f2b1546c3677ed989ca2",
+    "url": "/static/img/Logo-192x192.396936f.svg"
+  },
+  {
+    "revision": "1ab1a1fcab0ac52b1dcf5404d041629b",
+    "url": "/static/favicon.ico"
+  },
+  {
+    "revision": "9a7ee0ef52bde0703f90",
+    "url": "/static/css/app.0724d7bbedba6345a914af22454ef287.css"
+  },
+  {
+    "revision": "a9db6bb6923440dc2ac78c9215ce4046",
+    "url": "/index.html"
+  }
+];
